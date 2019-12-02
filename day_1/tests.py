8b0df05b868fe3c31d962e625988ea6462e8d9ba
Kerwin Connolly <kerwin.connolly@thehutgroup.com>
2019-12-02 16:36:01 +0000

    Refactor

--- day_1/tests.py
+++ day_1/tests.py
@@ -1,14 +1,14 @@
 import unittest
 
-from day_1 import puzzle
+from day_1 import run
 
 
 class TestDay1(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(3512133, puzzle.part_one())
+        self.assertEqual(3512133, run.part_one())
 
     def test_part_two(self):
-        self.assertEqual(5265294, puzzle.part_two())
+        self.assertEqual(5265294, run.part_two())
 
 
 if __name__ == '__main__':
--- day_2/tests.py
+++ day_2/tests.py
@@ -1,14 +1,14 @@
 import unittest
 
-from day_2 import puzzle
+from day_2 import run
 
 
 class TestDay2(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(4576384, puzzle.part_one())
+        self.assertEqual(4576384, run.part_one())
 
     def test_part_two(self):
-        self.assertEqual(5398, puzzle.part_two())
+        self.assertEqual(5398, run.part_two())
 
 
 if __name__ == '__main__':
