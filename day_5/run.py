ef58844d40d719344ca8613ae2f31e4ad65ebf0b
Kerwin Connolly <kerwin.connolly@thehutgroup.com>
2019-12-03 14:22:20 +0000

    Add template for day 5

---
+++ day_5/run.py
@@ -0,0 +1,11 @@
+def read(file):
+    with open(file, "r") as f:
+        pass
+
+
+def part_one():
+    pass
+
+
+def part_two():
+    pass
---
+++ day_5/tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+from day_5 import run
+
+
+class TestDay4(unittest.TestCase):
+    def test_part_one(self):
+        pass
+
+    def test_part_two(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
