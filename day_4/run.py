4c832b59913c4d7bcca942088a40a22688d42b45
Kerwin Connolly <kerwin.connolly@thehutgroup.com>
2019-12-04 13:49:24 +0000

    Refactor

--- day_4/run.py
+++ day_4/run.py
@@ -1,19 +1,28 @@
 from collections import Counter
 
 
+def get_passwords():
+    for p in range(382345, 843167):
+        return str(p)
+
+
 def is_ascending(p):
     return "".join(sorted(p)) == p
 
 
+def consecutive_counts(p):
+    return Counter(p).values()
+
+
 def part_one():
     return sum(
-        1 if is_ascending(str(p)) and max(Counter(str(p)).values()) > 1 else 0
-        for p in range(382345, 843167)
+        1 if is_ascending(str(p)) and max(consecutive_counts(p)) > 1 else 0
+        for p in get_passwords()
     )
 
 
 def part_two():
     return sum(
-        1 if is_ascending(str(p)) and 2 in Counter(str(p)).values() else 0
-        for p in range(382345, 843167)
+        1 if is_ascending(p) and 2 in consecutive_counts(p) else 0
+        for p in get_passwords()
     )
