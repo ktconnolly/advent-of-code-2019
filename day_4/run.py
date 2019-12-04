6599f7bbca35d0ae977a9b054a533ade647a9352
Kerwin Connolly <kerwin.connolly@thehutgroup.com>
2019-12-04 13:44:08 +0000

    Refactor

--- day_4/run.py
+++ day_4/run.py
@@ -1,19 +1,19 @@
 from collections import Counter
 
 
-def is_sorted(p):
-    return "".join(is_sorted(p)) == p
+def is_ascending(p):
+    return "".join(sorted(p)) == p
 
 
 def part_one():
     return sum(
-        1 if is_sorted(str(p)) and max(Counter(str(p)).values()) > 1 else 0
+        1 if is_ascending(str(p)) and max(Counter(str(p)).values()) > 1 else 0
         for p in range(382345, 843167)
     )
 
 
 def part_two():
     return sum(
-        1 if is_sorted(str(p)) and 2 in Counter(str(p)).values() else 0
+        1 if is_ascending(str(p)) and 2 in Counter(str(p)).values() else 0
         for p in range(382345, 843167)
     )
