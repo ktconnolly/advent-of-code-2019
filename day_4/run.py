6eb765ca0a420564a60a479f6d03495c8651b635
Kerwin Connolly <kerwin.connolly@thehutgroup.com>
2019-12-04 11:02:16 +0000

    Make more concise

--- day_4/run.py
+++ day_4/run.py
@@ -1,28 +1,19 @@
-from itertools import groupby
-
-
-def get_passwords():
-    for p in range(382345, 843167):
-        yield str(p)
+from collections import Counter
 
 
 def is_ascending(p):
     return all(x <= y for x, y in zip(p, p[1:]))
 
 
-def get_consecutive_counts(p):
-    return list(len(list(y)) for x, y in groupby(p))
-
-
 def part_one():
     return sum(
-        1 if is_ascending(p) and max(get_consecutive_counts(p)) > 1 else 0
-        for p in get_passwords()
+        1 if is_ascending(str(p)) and max(Counter(str(p)).values()) > 1 else 0
+        for p in range(382345, 843167)
     )
 
 
 def part_two():
     return sum(
-        1 if is_ascending(p) and 2 in get_consecutive_counts(p) else 0
-        for p in get_passwords()
+        1 if is_ascending(str(p)) and 2 in Counter(str(p)).values() else 0
+        for p in range(382345, 843167)
     )
