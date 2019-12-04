1530a48687b5da512325826612654df8017fe35e
Kerwin Connolly <kerwin.connolly@thehutgroup.com>
2019-12-04 10:19:59 +0000

    Complete day 4

--- day_4/run.py
+++ day_4/run.py
@@ -1,11 +1,28 @@
-def read(file):
-    with open(file, "r") as f:
-        pass
+from itertools import groupby
+
+
+def get_passwords():
+    for p in range(382345, 843167):
+        yield str(p)
+
+
+def is_ascending(p):
+    return all(x <= y for x, y in zip(p, p[1:]))
+
+
+def get_consecutive_counts(p):
+    return list(len(list(y)) for x, y in groupby(p))
 
 
 def part_one():
-    pass
+    return sum(
+        1 if is_ascending(p) and max(get_consecutive_counts(p)) > 1 else 0
+        for p in get_passwords()
+    )
 
 
 def part_two():
-    pass
+    return sum(
+        1 if is_ascending(p) and 2 in (get_consecutive_counts(p)) else 0
+        for p in get_passwords()
+    )
--- day_4/tests.py
+++ day_4/tests.py
@@ -5,10 +5,10 @@ from day_4 import run
 
 class TestDay4(unittest.TestCase):
     def test_part_one(self):
-        pass
+        self.assertEqual(460, run.part_one())
 
     def test_part_two(self):
-        pass
+        self.assertEqual(290, run.part_two())
 
 
 if __name__ == '__main__':
