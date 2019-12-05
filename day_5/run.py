815960051938265e080f8d33d66cca06764ee5c6
Kerwin Connolly <kerwin.connolly@thehutgroup.com>
2019-12-05 14:44:06 +0000

    Refactor

--- day_5/run.py
+++ day_5/run.py
@@ -3,60 +3,47 @@ def read(file):
         return [int(i) for i in f.readline().split(",")]
 
 
-def parse_instruction(instruction):
-    return [int(i) for i in str(instruction)[::-1]]
-
-
-def get_modes(instruction):
-    modes = [m for m in instruction[2:]]
-
-    while len(modes) < 3:
-        modes.append(0)
-
-    return modes
-
-
 def run_intcode(code):
     i = 0
     while code[i] != 99:
-        instruction = parse_instruction(code[i])
+        op = code[i] % 100
+        m1 = (code[i] // 100) % 10
+        m2 = (code[i] // 1000) % 10
+        m3 = (code[i] // 10000) % 10
 
-        opcode = instruction[0]
-        modes = get_modes(instruction)
+        p1 = i + 1 if m1 == 1 else code[i + 1]
+        p2 = i + 2 if m2 == 1 else code[i + 2]
+        p3 = i + 3 if m3 == 1 else code[i + 3]
 
-        p1 = i + 1 if modes[0] == 1 else code[i + 1]
-        p2 = i + 2 if modes[1] == 1 else code[i + 2]
-        p3 = i + 3 if modes[2] == 1 else code[i + 3]
-
-        if opcode == 1:
+        if op == 1:
             code[p3] = code[p1] + code[p2]
             i += 4
-        elif opcode == 2:
+        elif op == 2:
             code[p3] = code[p1] * code[p2]
             i += 4
-        elif opcode == 3:
+        elif op == 3:
             code[p1] = int(input("input: "))
             i += 2
-        elif opcode == 4:
+        elif op == 4:
             print(code[p1])
             i += 2
-        elif opcode == 5:
+        elif op == 5:
             if code[p1] != 0:
                 i = code[p2]
             else:
                 i += 3
-        elif opcode == 6:
+        elif op == 6:
             if code[p1] == 0:
                 i = code[p2]
             else:
                 i += 3
-        elif opcode == 7:
+        elif op == 7:
             if code[p1] < code[p2]:
                 code[p3] = 1
             else:
                 code[p3] = 0
             i += 4
-        elif opcode == 8:
+        elif op == 8:
             if code[p1] == code[p2]:
                 code[p3] = 1
             else:
@@ -69,4 +56,4 @@ def part_one():
 
 
 def part_two():
-    pass  # input 5 gives output of 508186
+    pass  # input 5 gives output of 3508186
