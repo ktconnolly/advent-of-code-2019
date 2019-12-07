9ec197d389b21457d8ae3e8aac42f5d2f1077882
Kerwin Connolly <kerwinconnolly@gmail.com>
2019-12-07 10:26:10 +0000

    Refactor

--- day_7/run.py
+++ day_7/run.py
@@ -11,12 +11,19 @@ class Program:
         self.code = code.copy()
         self.i = 0
 
+    def get_opcode(self):
+        return self.code[self.i] % 100
+
+    def get_modes(self):
+        m1 = (self.code[self.i] // 100) % 10
+        m2 = (self.code[self.i] // 1000) % 10
+        m3 = (self.code[self.i] // 10000) % 10
+        return m1, m2, m3
+
     def run(self, inputs):
         while self.i < len(self.code):
-            op = self.code[self.i] % 100
-            m1 = (self.code[self.i] // 100) % 10
-            m2 = (self.code[self.i] // 1000) % 10
-            m3 = (self.code[self.i] // 10000) % 10
+            op = self.get_opcode()
+            m1, m2, m3 = self.get_modes()
 
             if op == 99:
                 break
@@ -36,36 +43,45 @@ class Program:
             if op == 1:
                 self.code[p3] = self.code[p1] + self.code[p2]
                 self.i += 4
+
             elif op == 2:
                 self.code[p3] = self.code[p1] * self.code[p2]
                 self.i += 4
+
             elif op == 3:
                 self.code[p1] = inputs.pop(0)
                 self.i += 2
+
             elif op == 4:
                 self.i += 2
                 return self.code[p1]
+
             elif op == 5:
                 if self.code[p1] != 0:
                     self.i = self.code[p2]
                 else:
                     self.i += 3
+
             elif op == 6:
                 if self.code[p1] == 0:
                     self.i = self.code[p2]
                 else:
                     self.i += 3
+
             elif op == 7:
                 if self.code[p1] < self.code[p2]:
                     self.code[p3] = 1
                 else:
                     self.code[p3] = 0
+
                 self.i += 4
+
             elif op == 8:
                 if self.code[p1] == self.code[p2]:
                     self.code[p3] = 1
                 else:
                     self.code[p3] = 0
+
                 self.i += 4
 
 
