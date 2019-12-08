04a8458b3d041c6a03132576292fba68f1eaf4c1
Kerwin Connolly <kerwin.connolly@thehutgroup.com>
2019-12-08 13:28:32 +0000

    Refactor

--- day_8/run.py
+++ day_8/run.py
@@ -18,14 +18,8 @@ def get_colour(pixels):
 
 def part_one():
     layers = read("input.txt")
-    min_layer = None
-    min_zeros = WIDTH * HEIGHT
-    for layer in layers:
-        zeroes = layer.count("0")
-        if zeroes < min_zeros:
-            min_layer = layer
-            min_zeros = zeroes
-
+    zero_counts = {layer: layer.count("0") for layer in layers}
+    min_layer = min(zero_counts, key=zero_counts.get)
     return min_layer.count("1") * min_layer.count("2")
 
 
