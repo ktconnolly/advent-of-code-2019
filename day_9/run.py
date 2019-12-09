27cd4286c0fca745c07bd5eb99b675de67661af9
Kerwin Connolly <kerwin.connolly@thehutgroup.com>
2019-12-09 20:09:46 +0000

    Solve day 9

--- day_9/run.py
+++ day_9/run.py
@@ -7,27 +7,35 @@ class Computer:
     def __init__(self, code):
         self.code = code.copy()
         self.relative_base = 0
-        self.i = 0
+        self.ptr = 0
 
     def get_opcode(self):
-        return self.code[self.i] % 100
+        return self.code[self.ptr] % 100
 
     def get_mode(self, n):
-        return self.code[self.i] // (10 ** (1 + n)) % 10
+        return self.code[self.ptr] // (10 ** (1 + n)) % 10
 
     def get_param(self, param_num):
         mode = self.get_mode(param_num)
-        index = self.code[self.i + param_num]
+        index = self.code[self.ptr + param_num]
 
         if mode == 0:
             return self.code[index]
-        if mode == 1:
+        elif mode == 1:
             return index
-        if mode == 2:
+        elif mode == 2:
             return self.code[index + self.relative_base]
+        else:
+            raise Exception("Invalid mode")
 
-    def resize(self, n):
-        self.code += [0 for _ in range(n - len(self.code) + 1)]
+    def set_param(self, param_num, val):
+        mode = self.get_mode(param_num)
+        index = self.code[self.ptr + param_num]
+
+        if mode == 0:
+            self.write(index, val)
+        elif mode == 2:
+            self.write(index + self.relative_base, val)
 
     def write(self, index, val):
         if index > len(self.code) - 1:
@@ -35,17 +43,11 @@ class Computer:
 
         self.code[index] = val
 
-    def set_param(self, param_num, val):
-        mode = self.get_mode(param_num)
-        index = self.code[self.i + param_num]
-
-        if mode == 0:
-            self.write(index, val)
-        elif mode == 2:
-            self.write(index + self.relative_base, val)
+    def resize(self, index):
+        self.code += [0 for _ in range(index - len(self.code) + 1)]
 
     def run(self, inputs):
-        while self.i < len(self.code):
+        while self.ptr < len(self.code):
             op = self.get_opcode()
 
             if op == 99:
@@ -53,29 +55,29 @@ class Computer:
 
             if op == 1:
                 self.set_param(3, self.get_param(1) + self.get_param(2))
-                self.i += 4
+                self.ptr += 4
             elif op == 2:
                 self.set_param(3, self.get_param(1) * self.get_param(2))
-                self.i += 4
+                self.ptr += 4
             elif op == 3:
                 self.set_param(1, inputs.pop(0))
-                self.i += 2
+                self.ptr += 2
             elif op == 4:
                 yield self.get_param(1)
-                self.i += 2
+                self.ptr += 2
             elif op == 5:
-                self.i = self.get_param(2) if self.get_param(1) != 0 else self.i + 3
+                self.ptr = self.get_param(2) if self.get_param(1) != 0 else self.ptr + 3
             elif op == 6:
-                self.i = self.get_param(2) if self.get_param(1) == 0 else self.i + 3
+                self.ptr = self.get_param(2) if self.get_param(1) == 0 else self.ptr + 3
             elif op == 7:
                 self.set_param(3, int(self.get_param(1) < self.get_param(2)))
-                self.i += 4
+                self.ptr += 4
             elif op == 8:
                 self.set_param(3, int(self.get_param(1) == self.get_param(2)))
-                self.i += 4
+                self.ptr += 4
             elif op == 9:
                 self.relative_base += self.get_param(1)
-                self.i += 2
+                self.ptr += 2
             else:
                 raise Exception("Invalid opcode")
 
