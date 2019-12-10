44185657874a337806206dec00dbeb2d47fce19d
Kerwin Connolly <kerwin.connolly@thehutgroup.com>
2019-12-10 21:10:19 +0000

    Add day 11 template

---
+++ day_11/tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+from day_11 import run
+
+
+class TestDay11(unittest.TestCase):
+    def test_part_one(self):
+        pass
+
+    def test_part_two(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
