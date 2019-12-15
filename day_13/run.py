273a1ddc69e9c7239e056499694694face20aa2f
Kerwin Connolly <kerwinconnolly@gmail.com>
2019-12-15 15:31:57 +0000

    Move intcode to own file

--- day_13/run.py
+++ day_13/run.py
@@ -1,96 +1,9 @@
+from intcode import Computer
+
+
 def read(file):
     with open(file, "r") as f:
         return [int(i) for i in f.readline().split(",")]
-
-
-class Computer:
-    def __init__(self, code):
-        self.code = code.copy()
-        self.ptr = 0
-        self.relative_base = 0
-        self.inputs = []
-
-    def add_input(self, inp):
-        self.inputs.append(inp)
-
-    def get_opcode(self):
-        return self.code[self.ptr] % 100
-
-    def get_mode(self, n):
-        return self.code[self.ptr] // (10 ** (1 + n)) % 10
-
-    def get_param(self, param_num):
-        mode = self.get_mode(param_num)
-        index = self.code[self.ptr + param_num]
-
-        if mode == 0:
-            return self.read(index)
-        elif mode == 1:
-            return index
-        elif mode == 2:
-            return self.read(index + self.relative_base)
-        else:
-            raise Exception("Invalid mode")
-
-    def set_param(self, param_num, val):
-        mode = self.get_mode(param_num)
-        index = self.code[self.ptr + param_num]
-
-        if mode == 0:
-            self.write(index, val)
-        elif mode == 2:
-            self.write(index + self.relative_base, val)
-        else:
-            raise Exception("Invalid mode")
-
-    def read(self, index):
-        if index > len(self.code) - 1:
-            self.resize(index)
-
-        return self.code[index]
-
-    def write(self, index, val):
-        if index > len(self.code) - 1:
-            self.resize(index)
-
-        self.code[index] = val
-
-    def resize(self, index):
-        self.code += [0 for _ in range(index - len(self.code) + 1)]
-
-    def run(self):
-        while self.ptr < len(self.code):
-            op = self.get_opcode()
-            if op == 99:
-                break
-
-            if op == 1:
-                self.set_param(3, self.get_param(1) + self.get_param(2))
-                self.ptr += 4
-            elif op == 2:
-                self.set_param(3, self.get_param(1) * self.get_param(2))
-                self.ptr += 4
-            elif op == 3:
-                self.set_param(1, self.inputs.pop())
-                self.ptr += 2
-            elif op == 4:
-                yield self.get_param(1)
-                self.ptr += 2
-            elif op == 5:
-                self.ptr = self.get_param(2) if self.get_param(1) != 0 else self.ptr + 3
-            elif op == 6:
-                self.ptr = self.get_param(2) if self.get_param(1) == 0 else self.ptr + 3
-            elif op == 7:
-                self.set_param(3, int(self.get_param(1) < self.get_param(2)))
-                self.ptr += 4
-            elif op == 8:
-                self.set_param(3, int(self.get_param(1) == self.get_param(2)))
-                self.ptr += 4
-            elif op == 9:
-                self.relative_base += self.get_param(1)
-                self.ptr += 2
-            else:
-                raise Exception("Invalid opcode")
 
 
 class Game:
@@ -127,8 +40,9 @@ def part_one():
     computer = Computer(read("input.txt"))
     game = Game()
     outputs = []
-    for i in computer.run():
-        outputs.append(i)
+
+    while (output := computer.run()) is not None:
+        outputs.append(output)
 
         if len(outputs) == 3:
             tile = outputs.pop()
@@ -145,8 +59,8 @@ def part_two():
 
     game = Game()
     outputs = []
-    for i in computer.run():
-        outputs.append(i)
+    while (output := computer.run()) is not None:
+        outputs.append(output)
 
         if len(outputs) == 3:
             tile = outputs.pop()
--- day_15/run.py
+++ day_15/run.py
@@ -1,3 +1,6 @@
+from intcode import Computer
+
+
 WALL = 0
 SUCCESSFUL_MOVE = 1
 OXYGEN = 2
@@ -11,97 +14,6 @@ EAST = 4
 def read(file):
     with open(file, "r") as f:
         return [int(i) for i in f.readline().split(",")]
-
-
-class Computer:
-    def __init__(self, code):
-        self.code = code.copy()
-        self.ptr = 0
-        self.relative_base = 0
-        self.inputs = []
-
-    def add_input(self, inp):
-        self.inputs.append(inp)
-
-    def get_opcode(self):
-        return self.code[self.ptr] % 100
-
-    def get_mode(self, n):
-        return self.code[self.ptr] // (10 ** (1 + n)) % 10
-
-    def get_param(self, param_num):
-        mode = self.get_mode(param_num)
-        index = self.code[self.ptr + param_num]
-
-        if mode == 0:
-            return self.read(index)
-        elif mode == 1:
-            return index
-        elif mode == 2:
-            return self.read(index + self.relative_base)
-        else:
-            raise Exception("Invalid mode")
-
-    def set_param(self, param_num, val):
-        mode = self.get_mode(param_num)
-        index = self.code[self.ptr + param_num]
-
-        if mode == 0:
-            self.write(index, val)
-        elif mode == 2:
-            self.write(index + self.relative_base, val)
-        else:
-            raise Exception("Invalid mode")
-
-    def read(self, index):
-        if index > len(self.code) - 1:
-            self.resize(index)
-
-        return self.code[index]
-
-    def write(self, index, val):
-        if index > len(self.code) - 1:
-            self.resize(index)
-
-        self.code[index] = val
-
-    def resize(self, index):
-        self.code += [0 for _ in range(index - len(self.code) + 1)]
-
-    def run(self):
-        while self.ptr < len(self.code):
-            op = self.get_opcode()
-            if op == 99:
-                break
-
-            if op == 1:
-                self.set_param(3, self.get_param(1) + self.get_param(2))
-                self.ptr += 4
-            elif op == 2:
-                self.set_param(3, self.get_param(1) * self.get_param(2))
-                self.ptr += 4
-            elif op == 3:
-                self.set_param(1, self.inputs.pop())
-                self.ptr += 2
-            elif op == 4:
-                out = self.get_param(1)
-                self.ptr += 2
-                return out
-            elif op == 5:
-                self.ptr = self.get_param(2) if self.get_param(1) != 0 else self.ptr + 3
-            elif op == 6:
-                self.ptr = self.get_param(2) if self.get_param(1) == 0 else self.ptr + 3
-            elif op == 7:
-                self.set_param(3, int(self.get_param(1) < self.get_param(2)))
-                self.ptr += 4
-            elif op == 8:
-                self.set_param(3, int(self.get_param(1) == self.get_param(2)))
-                self.ptr += 4
-            elif op == 9:
-                self.relative_base += self.get_param(1)
-                self.ptr += 2
-            else:
-                raise Exception("Invalid opcode")
 
 
 def get_opposite(move):
